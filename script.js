01a0f99a96f7c84ea92a4812f291501e299e6bcd
Mahzan <110054003+mahzan123@users.noreply.github.com>
2023-12-22 06:23:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  //css.innerHTML = ".typewrite > .wrap { border-left: 0.08em solid #25d366}";
+  css.innerHTML = ".typewrite > .wrap { border-bottom: 0.2em solid #fff; border-radius: 5px; padding: 2px}";
+  document.body.appendChild(css);
+};
+
+//chat
+function chat() {
+  const message = `Halo! ${'Apa kabar'}`
+  const whatsapp = `https://wa.me/6285933650257?text=${message}`
+  open(whatsapp, "_blank")
+}
